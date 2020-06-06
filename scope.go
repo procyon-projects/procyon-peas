92ea779c943c255d9693c4b5e394419ed62d4dff
Burak Köken <burakkokenn@gmail.com>
2020-06-06 03:22:56 +0200

    Added PeaScope interface to define new scopes in your application.

---
+++ scope.go
@@ -0,0 +1,12 @@
+package peas
+
+/* If you don't implement PeaScope interface to your struct,
+ * it is considered as singleton.
+ */
+const (
+	PeaSingletonScope string = "singleton" /* it is only supported now by default */
+)
+
+type PeaScope interface {
+	GetScopeName() string
+}
